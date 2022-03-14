e0bc6d235a32d1865290eeaabd07d49c2fad5f85
Cass Middler <cassmiddler@Casss-MacBook-Air.local>
2022-03-14 20:16:46 +1100

    first

---
+++ main.py
@@ -0,0 +1,9 @@
+name=input("What is your name? \n\n")
+
+age=int(input("What is your age? \n\n"))
+
+if age >=18: 
+    print(f"Welcome {name}")
+else: 
+    print(f"Sorry, {name} Please try again another year")
+
